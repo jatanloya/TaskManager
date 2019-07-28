3aa2d4c07ed0ff30dd4da8b1d978e5c1a77ca00d
Jatan Loya <jatanloya@gmail.com>
2019-07-28 12:53:28 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from typing import Any
+
+from flask import Flask, escape, request, render_template, url_for, redirect
+
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+
+
+@app.route('/', methods=['POST','GET'])
+def index():
+    if request.method=='POST':
+        task_content=request.form['content']
+        new_task=ToDo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding your task"
+    else:
+        tasks=ToDo.query.order_by(ToDo.date_created).all()
+        return render_template("index.html", tasks=tasks)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
