fae462fafa00b3cad61a6303f4969592938c5577
Jatan Loya <jatanloya@gmail.com>
2019-07-28 19:33:33 +0530

    Delete functionality

--- app.py
+++ app.py
@@ -34,6 +34,16 @@ def index():
         tasks=ToDo.query.order_by(ToDo.date_created).all()
         return render_template("index.html", tasks=tasks)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = ToDo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+
+    except:
+        return 'There was a problem deleting the task'
 
 
 
